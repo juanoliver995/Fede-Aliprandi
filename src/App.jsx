1429925a06eab82720293d44f6aa20bd40ac3c69
Juan Oliver <juanoliver995@gmail.com>
2022-10-10 16:47:00 -0300

    first day

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react"
+// import { useState } from "react"
 import Nav from "./components/Nav"
 import Header from "./components/Header"
 import Slider from "./components/Slider"
 import Description from "./components/Description"
-import MusicList from "./components/MusicList"
+// import MusicList from "./components/MusicList"
 
 function App() {
-  const [trackListen, setTrackListen] = useState({})
+  // const [trackListen, setTrackListen] = useState({})
 
-  const handlePlay = (track) => {
-    if (trackListen.id !== track.id) {
-      setTrackListen(track)
-      setPlay(true)
-    }
-  }
+  // const handlePlay = (track) => {
+  //   if (trackListen.id !== track.id) {
+  //     setTrackListen(track)
+  //     setPlay(true)
+  //   }
+  // }
 
   return (
     <div className="container">
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <Slider />
       <Description />
-      <MusicList handlePlay={handlePlay} />
+      {/* <MusicList handlePlay={handlePlay} /> */}
 
 
     </div>
