d3d19d2245e93a51b3fe1ea635fa6e753f57c04d
Juan Oliver <juanoliver995@gmail.com>
2022-10-12 16:20:27 -0300

    arregle nav

--- src/styles/NavBarStyles.js
+++ src/styles/NavBarStyles.js
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 
 export const NavbarWrapper = styled.nav`
+  background-color: var(--dark-color);
   display: flex;
   flex-direction: column;
-  align-items: center;
   position: fixed;
   z-index: 1000;
   top: 0;
   right: ${props => (props.open ? "0" : "-100%")};
   transition: right 0.3s linear;
   height: 100vh;
-  padding-top: 3rem;
-  font-family: var(--title-font);
+  padding-top: 4rem;
+  font-family: var(--body-font);
+  text-transform: uppercase;
   width: 50%;
   a {
-    color: #fff;
+    color: var(--ligth-color);
     text-decoration: none;
-    font-size: 18px;
-    letter-spacing: 1px;
-    margin: 0 20px;
+    font-size: 20px;
+    margin-bottom: 10px;
+    margin-right: 10px;
   }
   @media only screen and (min-width: 920px) {
     width: 100%;
