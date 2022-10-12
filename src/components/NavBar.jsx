aa2ff22563017773981cc1536916b00165bcb6c4
Juan Oliver <juanoliver995@gmail.com>
2022-10-12 16:34:36 -0300

    Arregle nav en mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-scroll'
 function Navbar({ open, handleClick }) {
     return (
         <NavbarWrapper className="container-nav" open={open}>
-            <Link to="description" spy={true} smooth={true} offset={50} duration={500}>
+            <Link to="description" spy={true} smooth={true} offset={50} duration={500} onClick={handleClick}>
                 Description
             </Link>
-            <Link to="demos" spy={true} smooth={true} offset={50} duration={500}>
+            <Link to="demos" spy={true} smooth={true} offset={50} duration={500} onClick={handleClick}>
                 Demos
             </Link>
-            <Link to="packs" spy={true} smooth={true} offset={50} duration={500}>
+            <Link to="packs" spy={true} smooth={true} offset={50} duration={500} onClick={handleClick}>
                 Services
             </Link>
         </NavbarWrapper>
